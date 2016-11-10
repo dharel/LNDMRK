83d19d31c68a0651f5df6b90b672abd2e2968efd
carmit <carmit@spectory.com>
2016-11-10 12:16:18 +0200

    now started working with mock in scale 1- correct sizes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 
 Asset.create(
   [
-    { name: 'azrieli towers, tlv',  investment_type: 'income investment',          risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/001.jpg' },
-    { name: 'trump towers, nyc',    investment_type: 'income investment',          risk: 'c', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/002.jpg' },
-    { name: 'land for investment',  investment_type: 'growth investment',          risk: 'd', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/003.jpg' },
-    { name: 'ramat aviv mall, tlv', investment_type: 'income & growth investment', risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/004.jpg' },
-    { name: 'land for investment',  investment_type: 'growth investment',          risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/005.jpg' },
-    { name: 'azrieli towers, tlv',  investment_type: 'income investment',          risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/006.jpg' },
-    { name: 'trump towers, nyc',    investment_type: 'income investment',          risk: 'c', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/007.jpg' },
-    { name: 'land for investment',  investment_type: 'growth investment',          risk: 'd', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/008.jpg' },
-    { name: 'ramat aviv mall, tlv', investment_type: 'income & growth investment', risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/009.jpg' },
-    { name: 'land for investment',  investment_type: 'growth investment',          risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/010.jpg' }
+    { name: 'azrieli towers, tlv',  investment_type: 'income investment',          risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/001.png' },
+    { name: 'trump towers, nyc',    investment_type: 'income investment',          risk: 'c', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/002.png' },
+    { name: 'land for investment',  investment_type: 'growth investment',          risk: 'd', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/003.png' },
+    { name: 'ramat aviv mall, tlv', investment_type: 'income & growth investment', risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/004.png' },
+    { name: 'land for investment',  investment_type: 'growth investment',          risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/005.png' },
+    { name: 'azrieli towers, tlv',  investment_type: 'income investment',          risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/001.png' },
+    { name: 'trump towers, nyc',    investment_type: 'income investment',          risk: 'c', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/002.png' },
+    { name: 'land for investment',  investment_type: 'growth investment',          risk: 'd', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/003.png' },
+    { name: 'ramat aviv mall, tlv', investment_type: 'income & growth investment', risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/004.png' },
+    { name: 'land for investment',  investment_type: 'growth investment',          risk: 'b', price: '10', income: '20', yeild: '7.1', image: '/investment_assets/005.png' }
   ]
 )
