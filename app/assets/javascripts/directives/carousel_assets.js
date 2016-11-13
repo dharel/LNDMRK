f926ce87456aa17f22617620e4a35c7036717f9d
carmit <carmit@spectory.com>
2016-11-13 17:01:00 +0200

    carousel adjust to new mock #1

--- app/assets/javascripts/directives/carousel_assets.js
+++ app/assets/javascripts/directives/carousel_assets.js
@@ -11,24 +11,26 @@ angular.module('lndmrk').directive('carouselAssets', ['$timeout', function ($tim
     "<div class='asset-block'>" +
       "<a href='#'>" +
       "<h3>{{asset.name}}</h3>" +
-      "<h4>{{asset.investment_type}}</h4>" +
       
       "<div class='box-wrap'>" +
-        "<div class='right-col'>" +
-          "<div class='risk-letter'>{{asset.risk}}</div>" +
-          "<div class='info-title'>Price per m&sup2;</div>" +
-          "<div class='info-data'>${{asset.price}}K</div>" +
-          "<div class='info-title'>Income per m&sup2;</div>" +
-          "<div class='info-data'>${{asset.income}}</div>" +
-          "<div class='info-title'>Yeild</div>" +
-          "<div class='info-data'>{{asset.yeild}}%</div>" +
+        "<div class='row'>" +
+          "<div class='right-col'>" +
+            "<div class='risk-letter'>{{asset.risk}}</div>" +
+            "<div class='info-title'>Price per m&sup2;</div>" +
+            "<div class='info-data'>${{asset.price}}K</div>" +
+            "<div class='info-title'>Income per m&sup2;</div>" +
+            "<div class='info-data'>${{asset.income}}</div>" +
+            "<div class='info-title'>Yeild</div>" +
+            "<div class='info-data'>{{asset.yeild}}%</div>" +
+          "</div>" +
+          "<div class='image-placer'>" +
+            "<img src='{{asset.image}}' width='124' alt='asset'>" +
+            "<div class='invest'><span>invest</span></div>" +
+          "</div>" +
         "</div>" +
-
-        "<div class='image-placer'>" +
-          "<img src='{{asset.image}}' width='124' alt='asset'>" +
-          "<div class='invest'><span>invest</span></div>" +
+        "<div class='bottom-row'" +
+          "<h4>{{asset.investment_type}}</h4>" +
         "</div>" +
-
         "<div class='image-border'></div>" +
       "</div>" +
       "</a>" +
