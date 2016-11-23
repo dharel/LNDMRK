5354165835fd799100d2dc6f888b9afd096232d2
zvikae <zvi@spectory.com>
2016-11-23 14:44:59 +0200

    search properties block

--- app/assets/javascripts/controllers/search_controller.js
+++ app/assets/javascripts/controllers/search_controller.js
@@ -1,6 +1,19 @@
 /*globals angular , window, unused, _  */
 angular.module('lndmrk').controller('SearchController', ['$scope', 'AjaxService','$translate','localizationSrv', function ($scope, AjaxService, $translate, localizationSrv) {
   'use strict';
+  $scope.init = function () {
+    $scope.market_type_checkbox_prime = {
+      checked: false
+    };
 
+    $scope.market_type_checkbox_fringe = {
+      checked: false
+    };
+
+    $scope.market_type_checkbox_secondary = {
+      checked: false
+    };
+
+  };
 
 }]);
