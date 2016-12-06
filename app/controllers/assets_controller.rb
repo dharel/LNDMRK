ea1109f2d226d22516de588d6211b920409e65aa
carmit <carmit@spectory.com>
2016-12-06 09:09:56 +0200

    code clean from drafts

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -7,21 +7,9 @@ class AssetsController < ApplicationController
   def parsed_assets
     types = InvestmentType.all.as_json
     assets = { owned_assets: [], watched_assets: [] }
-
     types.each do |type|
-      # type['assets'] = Asset.all.where(investment_type: type['name']).as_json
       type['assets'] = Asset.all.where(investment_type: type['name'], user_owned: true).as_json
-      # byebug
-      # assets[:owned_assets].push({ name:'carmit' })
-      # assets[:watched_assets].push({ name:'yellow' })
-      
-      # assets[:owned_assets].push(Asset.all.where(investment_type: type['name'], user_owned: true).as_json)
-      # assets[:watched_assets].push(Asset.all.where(investment_type: type['name'], user_watched: true).as_json)
-      
-      # type['owned_assets'] = Asset.all.where(investment_type: type['name'], user_owned: true).as_json
-      # type['watched_assets'] = Asset.all.where(investment_type: type['name'], user_watched: true).as_json
     end
-
     render json: assets
   end
 
@@ -42,9 +30,6 @@ class AssetsController < ApplicationController
   def remove_from_watchlist
     asset = Asset.find(params[:asset_id])
     asset.update(user_watched: false)
-
-    # watched_assets = Asset.all.where(user_watched: true).as_json
-    # render json: watched_assets
     render json: { status: 'removed asset from watchlist' }
   end
 
